93cba9c6fba5a3b4fe77d9e0ab4c7b878a7b31d7
karthik <karthik3116k@gmail.com>
2025-04-28 01:23:50 +0530

    Hardcoded db URI

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 const connectDB = async()=>{
 
     try{
-        const connect = await mongoose.connect(process.env.MONGODB_URI)
+        const connect = await mongoose.connect("mongodb+srv://karthik3116k:SDTo3NAOVYvHakrm@cluster0.edbxe4c.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0")
         console.log("database connected sucessfulu :) ");
     }catch(err){
         console.log("error connecting to database :( ", err);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // no array needed unless multiple origins
+    origin: "https://chat.karthik.top", // no array needed unless multiple origins
     credentials: true, // allow cookies/auth headers if needed
   },
 });
