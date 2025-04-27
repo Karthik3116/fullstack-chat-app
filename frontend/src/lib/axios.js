4a79476fb037891a69b847766ce5049dc144b976
karthik <karthik3116k@gmail.com>
2025-04-28 00:15:18 +0530

    Fixed Avatar

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,6 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     baseURL : import.meta.MODE === "development" ?  "http://localhost:5001/api":"/api",
+    // baseURL : "http://localhost:5001/api",
     withCredentials : true //for sending cookies
 });
