88157d861bb565bcd01466c3fe97d8c51e9db9c7
karthik <karthik3116k@gmail.com>
2025-04-28 00:52:38 +0530

    Changed Backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.MODE === "development" ?  "http://localhost:5001/api":"/api",
+    baseURL : "https://chatbackend.karthik.top/api",
     // baseURL : "http://localhost:5001/api",
     withCredentials : true //for sending cookies
 });
