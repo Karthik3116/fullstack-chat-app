573c09809160c44fd67cba453920d7809f2b3b02
karthik <karthik3116k@gmail.com>
2025-04-28 00:25:54 +0530

    Fixed index

--- backend/src/index.js
+++ backend/src/index.js
@@ -73,12 +73,12 @@ app.use("/api/messages", messageRouts);
 console.log("🚀 Registered Routes:");
 
  
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*splat", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("/*splat", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT || 5001, () => {
   console.log("IO Server is running on port : " + (PORT || 5001));
