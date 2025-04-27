ba4cff3714e01d797b858a64ee690a8e8c655a6a
karthik <karthik3116k@gmail.com>
2025-04-27 21:29:41 +0530

    Done index /*splat

--- backend/src/index.js
+++ backend/src/index.js
@@ -83,6 +83,14 @@ console.log("🚀 DB Connected ");
 
 // export default app; 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
 server.listen(PORT || 5001, () => {
   console.log("IO Server is running on port : " + (PORT || 5001));
 });
