e390a1deef6e4cda6adbc6f723dec54e45daa8b8
karthik <karthik3116k@gmail.com>
2025-04-29 01:27:29 +0530

    removed jwt and fixed ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,71 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-import { Routes , Route, Navigate } from "react-router-dom"
-import Navbar  from "./components/Navbar"
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignUpPage";
+import CheckPage from "./pages/CheckPage";
+import NotFoundPage from "./components/NotFoundPage";
 
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
-import Homepage from "./pages/Homepage"
-import LoginPage from "./pages/LoginPage"
-import SettingsPage from "./pages/SettingsPage"
-import ProfilePage from "./pages/ProfilePage"
-import SignUpPage from "./pages/SignUpPage"
-import CheckPage from "./pages/checkPage"
-import { axiosInstance } from "./lib/axios"
-import { useAuthStore } from "./store/useAuthStore"
-import { useThemeStore } from "./store/useThemeStore"
-import { useEffect } from "react"
-
-import {Loader} from "lucide-react";
-
-import {Toaster} from "react-hot-toast"
-import NotFoundPage from "./components/NotFoundPage"
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-  const {authUser, checkAuth , isCheckingAuth , onlineUsers} = useAuthStore();
-  
-  console.log("Online Users are :- " ,onlineUsers);
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-  const {theme} = useThemeStore();
-  useEffect(() =>{
-    checkAuth()
-  },[checkAuth])
-
-  console.log({authUser});
-  if((isCheckingAuth && !authUser)){
+  if (isCheckingAuth) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin"/>
+        <Loader className="size-10 animate-spin" />
       </div>
-    )
+    );
   }
 
   return (
-      <div data-theme = {theme}>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={ authUser ? <Homepage/> : <Navigate to="/login"/>}/>
-          <Route path="/signup" element={!authUser ?<SignUpPage/> : <Navigate to="/"/>}/>
-          <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
-          <Route path="/settings" element={<SettingsPage/>}/>
-          <Route path="/profile" element={authUser ?<ProfilePage/> : <Navigate to="/login"/>}/>
-          <Route path="/check" element={ <CheckPage/>}/>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+    <div data-theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Homepage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/settings"
+          element={authUser ? <SettingsPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/check"
+          element={<CheckPage />}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
 
-        <Toaster/>
-      </div>
-      
-    
-  )
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
