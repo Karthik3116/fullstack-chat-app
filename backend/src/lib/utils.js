ee6b41b7583bb6807472ed6997695964e84002d8
karthik <karthik3116k@gmail.com>
2025-04-29 00:08:06 +0530

    Fixed utils.js in server

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,8 +9,8 @@ const genToken = (userId , res) =>{
     res.cookie("jwt",token , {
         maxAge : 7*24*60*60*1000,
         httpOnly:true,
-        sameSite:"strict",
-        secure : process.env.NODE_ENV !== "development"
+        sameSite:"Lax",
+        secure : true
     });
 
     return token
