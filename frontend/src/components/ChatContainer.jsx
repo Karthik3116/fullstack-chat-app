2a8483516dd05ccfd8a42dd86e4564db4cb351bf
karthik <karthik3116k@gmail.com>
2025-04-29 15:07:33 +0530

    added send dim logic

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -96,6 +96,130 @@
 // export default ChatContainer;
 
 
+// import React, { useEffect, useRef } from "react";
+// import { useChatStore } from "../store/useChatStore";
+// import { useAuthStore } from "../store/useAuthStore";
+
+// import ChatHeader from "./ChatHeader";
+// import MessageInput from "./MessageInput";
+// import MessageSkeleton from "./skeletons/MessageSkeleton";
+// import { formatMessageTime } from "../lib/utils";
+
+// const ChatContainer = () => {
+//   const {
+//     messages,
+//     getMessages,
+//     isMessagesLoading,
+//     selectedUser,
+//     subscribeToMessages,
+//     unsubscribeFromMessages,
+//   } = useChatStore();
+
+//   const { authUser } = useAuthStore();
+//   const messageEndRef = useRef(null);
+
+//   // 1) fetch & subscribe on user change
+//   useEffect(() => {
+//     getMessages(selectedUser._id);
+//     subscribeToMessages();
+//     return () => unsubscribeFromMessages();
+//   }, [
+//     selectedUser._id,
+//     getMessages,
+//     subscribeToMessages,
+//     unsubscribeFromMessages,
+//   ]);
+
+//   // 2) scroll fully to bottom on any messages update
+//   useEffect(() => {
+//     if (messageEndRef.current) {
+//       messageEndRef.current.scrollIntoView({
+//         behavior: "smooth",
+//         block: "end",
+//       });
+//     }
+//   }, [messages]);
+
+//   if (isMessagesLoading) {
+//     return (
+//       <div className="flex-1 flex flex-col overflow-auto">
+//         <ChatHeader />
+//         <MessageSkeleton />
+//         <MessageInput />
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="flex-1 flex flex-col overflow-auto">
+//       <ChatHeader />
+
+//       <div className="flex-1 overflow-y-auto p-4 space-y-4">
+//         {messages.map((message) => (
+//           <div
+//             key={message._id}
+//             className={`chat ${
+//               message.senderId === authUser._id ? "chat-end" : "chat-start"
+//             }`}
+//           >
+//             <div className="chat-image avatar">
+//               <div className="size-10 rounded-full border">
+//                 <img
+//                   src={
+//                     message.senderId === authUser._id
+//                       ? authUser.profilePic ||
+//                         "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+//                       : selectedUser.profilePic ||
+//                         "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+//                   }
+//                   alt="profile pic"
+//                   onError={(e) => {
+//                     e.currentTarget.onerror = null;
+//                     e.currentTarget.src =
+//                       "https://cdn-icons-png.flaticon.com/512/847/847969.png";
+//                   }}
+//                 />
+//               </div>
+//             </div>
+
+//             <div className="chat-header mb-1">
+//               <time className="text-xs opacity-50 ml-1">
+//                 {formatMessageTime(message.createdAt)}
+//               </time>
+//             </div>
+
+//             <div className="chat-bubble flex flex-col">
+//               {message.image && (
+//                 <img
+//                   src={message.image}
+//                   alt="Attachment"
+//                   className="sm:max-w-[200px] rounded-md mb-2"
+//                   onLoad={() => {
+//                     // re-scroll once the image has rendered
+//                     messageEndRef.current?.scrollIntoView({
+//                       behavior: "smooth",
+//                       block: "end",
+//                     });
+//                   }}
+//                 />
+//               )}
+//               {message.text && <p>{message.text}</p>}
+//             </div>
+//           </div>
+//         ))}
+
+//         {/* sentinel div */}
+//         <div ref={messageEndRef} />
+//       </div>
+
+//       <MessageInput />
+//     </div>
+//   );
+// };
+
+// export default ChatContainer;
+
+
 import React, { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
@@ -145,7 +269,7 @@ const ChatContainer = () => {
       <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
         <MessageSkeleton />
-        <MessageInput />
+        <MessageInput isSending={false} />
       </div>
     );
   }
@@ -195,7 +319,6 @@ const ChatContainer = () => {
                   alt="Attachment"
                   className="sm:max-w-[200px] rounded-md mb-2"
                   onLoad={() => {
-                    // re-scroll once the image has rendered
                     messageEndRef.current?.scrollIntoView({
                       behavior: "smooth",
                       block: "end",
